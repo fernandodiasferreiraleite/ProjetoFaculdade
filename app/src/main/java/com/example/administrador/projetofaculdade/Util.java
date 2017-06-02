3a8c8dbaf023e6aaa420962a6a2c47e53341727c
fernando <fernandodiasferreiraleite@hotmail.com>
2017-06-02 18:37:13 -0300

    Manutencao classe util e ListDespesa.

--- app/src/main/java/com/example/administrador/projetofaculdade/Util.java
+++ app/src/main/java/com/example/administrador/projetofaculdade/Util.java
@@ -15,6 +15,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import static android.R.attr.format;
+
 
 /**
  * Created by Lab. Desenvolvimento on 03/04/2017.
@@ -44,17 +46,16 @@ public class Util {
         return localString;
     }
 
-    public static String convertDespesatoJSON(Despesa despesa){
+    public static String convertDespesatoJSON(Despesa despesa) {
         JSONObject mainObject = new JSONObject();
         try {
-            mainObject.put("nomeDespesa",despesa.getNomeDespesa());
-            mainObject.put("dataPagamento",despesa.getDataPagamento());
-            mainObject.put("desconto",despesa.getDesconto());
-            mainObject.put("id_despesa",despesa.getIdDespesa());
-            mainObject.put("idpessoa",despesa.getIdPessoa());
-            mainObject.put("idtipodespesa",despesa.getIdTipoDespesa());
-            mainObject.put("valor",despesa.getValor());
-
+            mainObject.put("nomeDespesa", despesa.getNomeDespesa());
+            mainObject.put("dataPagamento", despesa.getDataPagamento());
+            mainObject.put("desconto", despesa.getDesconto());
+            mainObject.put("idDespesa", despesa.getIdDespesa());
+            mainObject.put("idPessoa", despesa.getIdPessoa());
+            mainObject.put("idTipoDespesa", despesa.getIdTipoDespesa());
+            mainObject.put("valor", despesa.getValor());
             return mainObject.toString();
         } catch (JSONException e) {
             e.printStackTrace();
@@ -63,38 +64,36 @@ public class Util {
     }
 
 
+    public static List<Despesa> convertJSONtoDespesa(String jsonFile) {
 
-    public static List<Despesa> convertJSONtoDespesa(String jsonFile){
-
-        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        //SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
 
         List<Despesa> despesas = new ArrayList<>();
         try {
             JSONArray mainObject = new JSONArray(jsonFile);
 
-            for(int i = 0; i < mainObject.length(); i++){
+            for (int i = 0; i < mainObject.length(); i++) {
 
                 Despesa novoDespesa = new Despesa();
-
                 JSONObject localObj = mainObject.getJSONObject(i);
 
-                int IdDespesa = localObj.getInt("IdDespesa");
+                int id = localObj.getInt("idDespesa");
 
-                String nomeDespesa = localObj.getString("nomeDespesa");
+                String despesa = localObj.getString("nomeDespesa");
 
                 float valor = Float.parseFloat(localObj.getString("valor"));
 
-                Date dataPagamento = formato.parse("dataPagamento");
+                String dataPagamento = localObj.getString("dataPagamento");
 
                 float desconto = Float.parseFloat(localObj.getString("desconto"));
 
-                int idTipoDespesa = localObj.getInt("IdTipoDespesa");
+                int idTipoDespesa = localObj.getInt("idTipoDespesa");
 
-                int idPessoa = localObj.getInt("IdPessoa");
+                int idPessoa = localObj.getInt("idPessoa");
 
-                novoDespesa.setIdDespesa(IdDespesa);
+                novoDespesa.setIdDespesa(id);
 
-                novoDespesa.setNomeDespesa(nomeDespesa);
+                novoDespesa.setNomeDespesa(despesa);
 
                 novoDespesa.setValor(valor);
 
@@ -111,12 +110,11 @@ public class Util {
 
         } catch (JSONException e) {
             e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
         }
         return despesas;
     }
-    public static String getStatusFromJSOM(String json){
+
+    public static String getStatusFromJSOM(String json) {
         try {
             return new JSONObject(json).getString("status");
         } catch (JSONException e) {
@@ -124,8 +122,4 @@ public class Util {
         }
         return null;
     }
-
-
-
-
 }
